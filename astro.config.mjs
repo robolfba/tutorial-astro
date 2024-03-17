a3cebc4f64ec960a73a24073803436ab58df4ab0
robolfba <robolfba@gmail.com>
2024-03-17 15:25:18 -0300

    Se agrega preact como dependencia del proyecto. Se implementa isla interactiva.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({site:'https://rrobol-tutorial-astro.netlify.app/'});
+export default defineConfig({
+  site: 'https://rrobol-tutorial-astro.netlify.app/',
+  integrations: [preact()]
+});
